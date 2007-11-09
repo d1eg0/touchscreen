2e1264c4a69ceb6505ffa37b7e8998d6850f706f
kobydiego <kobydiego@19c347df-c239-0410-ab3b-cf5cc4a55563>
2007-11-09 11:13:21 +0000

    todo bien, sin surfaces

--- src/dibujar.cpp
+++ src/dibujar.cpp
@@ -32,6 +32,7 @@ void Dibujar::dibujarLinea(Frame *frame, Linea *linea, Uint32 color) {
     y1=linea->getY1();
     y2=linea->getY2();
     SDL_mutexP(semVideo);
+    frame->activar();
     if(SDL_MUSTLOCK(screen))SDL_LockSurface(screen);
     lineColor(screen, (Sint16)x1, (Sint16)y1, (Sint16)x2, (Sint16)y2, color);
     if(SDL_MUSTLOCK(screen))SDL_UnlockSurface(screen);
